4fcf46d027f04a61a094c66653848fa1abf045bc
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-10-17 23:33:37 +0330

    feat: Define file types and corresponding folders for organization

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import shutil
 
 # Desktop path
 desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+
+# Definition of formats and folders for each type of file
+file_types = {
+    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'],
+    'Music': ['.mp3', '.wav', '.flac', '.aac', '.ogg'],
+    'Documents': ['.pdf', '.doc', '.docx', '.txt', '.ppt', '.pptx', '.xls', '.xlsx'],
+    'Videos': ['.mp4', '.avi', '.mkv', '.mov', '.wmv'],
+    'Compressed': ['.zip', '.rar', '.tar', '.gz'],
+}
+
